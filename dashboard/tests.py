c3364f211995b0ea6a68bd17b75f8fe346a6e119
nasirhaidary <sn.haidary@gmail.com>
2025-05-11 18:22:02 +0200

    Updated .gitignore to clarify ignored environment files and added missing newline. Enhanced tests with a simple addition test case in both assets and dashboard test files. Added static context processor in settings.py for improved static file handling.

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+# This is a simple test case to check if 1 + 1 equals 2
+class SimpleTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
--- it_asset_tracker/settings.py
+++ it_asset_tracker/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
+                'django.template.context_processors.static',  # Added for static files (From Nasir).
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
